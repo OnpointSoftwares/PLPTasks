04b21e3a5cb692f92bc8cda273ab691cacc23b42
OnpointSoftwares <vincentbettoh@gmail.com>
2024-05-23 07:55:21 -0400

    DataFlowControl Assignment for flutter course week 3

---
+++ controlFlowAssignment.dart
@@ -0,0 +1,15 @@
+void main()
+{
+  int a=12;
+  if(a==10)
+  {
+    print("Your number is equal to 10");
+  }
+  else if(a>10)
+  {
+    print("Your number is greater than 10");
+  }
+  else{
+    print("Your number is equal to 10");
+  }
+}
